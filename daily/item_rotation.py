52d759fff5e6a18e6381255b6b0c7803d6521364
Pomfret <Pomfret27@outlook.com>
2026-07-01 23:07:48 +0800

    ✨ feat(daily): 新增物品轮换脚本

---
+++ daily/item_rotation.py
@@ -0,0 +1,25 @@
+from datetime import date
+
+def get_item(start_date: date, target_date: date, item_list: list):
+    interval = (target_date - start_date).days  # 计算日期间隔
+    cycle_num = len(item_list)                  # 获取轮转周期
+    item_index = interval % cycle_num           # 计算物品序号
+    return item_list[item_index]
+
+if __name__ == "__main__":
+
+    cycle_start_date = date(2026, 3, 9)         # 轮转开始日期
+
+    # 物品列表格式：[["物品名称"], ["特征名称"], ["特征值"]]
+    item_a = [["内裤"], ["颜色"], ["灰-1", "浅蓝", "黑色", "深蓝", "灰-2"]]
+    item_b = [["袜子"], ["颜色"], ["白色", "浅灰", "蓝色", "深灰", "黑色"]]
+
+    today_date = date.today()                   # 获取今日日期
+
+    # 获取今日物品特征值
+    today_a = get_item(cycle_start_date, today_date, item_a[2])
+    today_b = get_item(cycle_start_date, today_date, item_b[2])
+
+    # 输出格式："物品名称""特征名称"："特征值"
+    print(f"{item_a[0][0]}{item_a[1][0]}：{today_a}")
+    print(f"{item_b[0][0]}{item_b[1][0]}：{today_b}")
