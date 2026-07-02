b4e656a0953ee4220d3a64cb312b776bbcf0b7e6
Pomfret <Pomfret27@outlook.com>
2026-07-02 22:47:52 +0800

    🎈 perf(优化物品轮换脚本): 新增明日物品输出

--- daily/item_rotation.py
+++ daily/item_rotation.py
@@ -1,7 +1,9 @@
 from datetime import date
 
-def get_item(start_date: date, target_date: date, item_list: list):
+def get_item(start_date: date, target_date: date, item_list: list, tomorrow: bool):
     interval = (target_date - start_date).days  # 计算日期间隔
+    if tomorrow:                                # 判断今日明日
+        interval += 1                           # 明日间隔加一
     cycle_num = len(item_list)                  # 获取轮转周期
     item_index = interval % cycle_num           # 计算物品序号
     return item_list[item_index]
@@ -17,9 +19,13 @@ if __name__ == "__main__":
     today_date = date.today()                   # 获取今日日期
 
     # 获取今日物品特征值
-    today_a = get_item(cycle_start_date, today_date, item_a[2])
-    today_b = get_item(cycle_start_date, today_date, item_b[2])
+    today_a = get_item(cycle_start_date, today_date, item_a[2], False)
+    today_b = get_item(cycle_start_date, today_date, item_b[2], False)
+
+    # 获取明日物品特征值
+    tomorrow_a = get_item(cycle_start_date, today_date, item_a[2], True)
+    tomorrow_b = get_item(cycle_start_date, today_date, item_b[2], True)
 
     # 输出格式："物品名称""特征名称"："特征值"
-    print(f"{item_a[0][0]}{item_a[1][0]}：{today_a}")
-    print(f"{item_b[0][0]}{item_b[1][0]}：{today_b}")
+    print(f"今日{item_a[0][0]}{item_a[1][0]}：{today_a}\n今日{item_b[0][0]}{item_b[1][0]}：{today_b}\n")
+    print(f"明日{item_a[0][0]}{item_a[1][0]}：{tomorrow_a}\n明日{item_b[0][0]}{item_b[1][0]}：{tomorrow_b}\n")
